7a87354c399fc715da74d130feb25e395d82e869
adam139 <yuejun.tang@gmail.com>
2019-08-03 10:11:53 +0800

    add CreateMembraneEvent

--- my315ok/socialorgnization/events.py
+++ my315ok/socialorgnization/events.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from my315ok.socialorgnization.interfaces import ICreateOrgEvent
+from my315ok.socialorgnization.interfaces import ICreateMembraneEvent
 from zope import interface
 from zope.component import adapts
 from zope.component.interfaces import ObjectEvent
@@ -22,3 +23,23 @@ class CreateOrgEvent(object):
         self.organization_type = organization_type
         self.announcement_type = announcement_type
         self.passDate = passDate
+
+
+class CreateMembraneEvent(object):
+    interface.implements(ICreateMembraneEvent)
+
+    def __init__(self,id,email,password,title,description,homepage,phone,organization,\
+                 sector,position,province,address):
+        """角色,级别,备注"""
+        self.id = id
+        self.email = email
+        self.password = password
+        self.title = title
+        self.description = description
+        self.homepage = homepage
+        self.phone = phone
+        self.organization = organization
+        self.sector = sector
+        self.position = position
+        self.province = province
+        self.address = address
--- my315ok/socialorgnization/interfaces.py
+++ my315ok/socialorgnization/interfaces.py
@@ -5,7 +5,8 @@ from zope.interface import Interface
 
 
 # event
-
+class ICreateMembraneEvent(Interface):
+    """新增一个organization member object"""
 
 class ICreateOrgEvent(Interface):
     """新增一个organization object"""
